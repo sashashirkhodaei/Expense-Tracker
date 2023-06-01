0d6951855066047af238629d53238a1e249036e3
Sasha Shirkhodaei <123327900+sashashirkhodaei@users.noreply.github.com>
2023-06-01 16:41:51 -0700

    Add income input functionality

--- app.py
+++ app.py
@@ -27,3 +27,8 @@ with st.form("entry_form", clear_on_submit=True):
     col1, col2 = st.columns(2)
     col1.selectbox("Select Month:", months, key="month")
     col2.selectbox("Select Year:", years, key="year")
+           
+    "---"
+    with st.expander("Income"):
+        for income in incomes:
+            st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
