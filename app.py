c65725c557bc8e46f70009f9df8409977463b214
Sasha Shirkhodaei <123327900+sashashirkhodaei@users.noreply.github.com>
2023-06-01 15:51:48 -0700

    set up / define settings & main functions

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-import plotly.graph_objects as go # pip install plotly
-import streamlit as st # pip install streamlit
+import ploty.graph_objects as go # conda install plotly
+import streamlit as st # conda install streamlit
+
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Investments", "Other Income"]
+expenses = ["Rent", "Utilities", "Food", "Transportation", "Miscellaneous",
+            "Phone", "Savings", "Insurance", "Subscriptions"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"
+layout = "centered"
+# --------------------------------------
