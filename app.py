41e1f461865701204bd94ad0961bb6cb419a5dfc
Sasha Shirkhodaei <123327900+sashashirkhodaei@users.noreply.github.com>
2023-06-01 18:00:39 -0700

    remove default streamilt menu, header, & footer

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ st.title(page_title + " " + page_icon)
 years = [datetime.today().year, datetime.today().year + 1]
 months = list(calendar.month_name[1:])
 
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
