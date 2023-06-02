918b4801174a1019d2eb91887b7a561853afe821
Sasha Shirkhodaei <123327900+sashashirkhodaei@users.noreply.github.com>
2023-06-01 17:58:11 -0700

    made navigation functionable

--- app.py
+++ app.py
@@ -32,65 +32,67 @@ selected = option_menu(
 )
 
 # --- INPUT & SAVE PERIODS ---
-st.header(f"Data Entry in {currency}")
-with st.form("entry_form", clear_on_submit=True):
-    col1, col2 = st.columns(2)
-    col1.selectbox("Select Month:", months, key="month")
-    col2.selectbox("Select Year:", years, key="year")
-           
-    "---"
-    with st.expander("Income"):
-        for income in incomes:
-            st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
-    with st.expander("Expenses"):
-        for expense in expenses: 
-            st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
-    with st.expander("Comment"):
-        comment = st.text_area("", placeholder="Enter a comment here ...")
-    
-    "---"
-    submitted = st.form_submit_button("Save Data")
-    if submitted:
-        period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
-        incomes = {income: st.session_state[income] for income in incomes}
-        expenses = {expense: st.session_state[expense] for expense in expenses}
-        st.write(f"incomes: {incomes}")
-        st.write(f"expenses: {expenses}")
-        st.success("Data Saved!")
+if selected == "Data Entry":
+    st.header(f"Data Entry in {currency}")
+    with st.form("entry_form", clear_on_submit=True):
+        col1, col2 = st.columns(2)
+        col1.selectbox("Select Month:", months, key="month")
+        col2.selectbox("Select Year:", years, key="year")
+            
+        "---"
+        with st.expander("Income"):
+            for income in incomes:
+                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
+        with st.expander("Expenses"):
+            for expense in expenses: 
+                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+        with st.expander("Comment"):
+            comment = st.text_area("", placeholder="Enter a comment here ...")
+        
+        "---"
+        submitted = st.form_submit_button("Save Data")
+        if submitted:
+            period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+            incomes = {income: st.session_state[income] for income in incomes}
+            expenses = {expense: st.session_state[expense] for expense in expenses}
+            st.write(f"incomes: {incomes}")
+            st.write(f"expenses: {expenses}")
+            st.success("Data Saved!")
 
 # --- PLOT PERIODS ---
-st.header("Data Visualization")
-with st.form("saved_periods"):
-    period = st.selectbox("Select Period:", ["June_2023"])
-    submitted = st.form_submit_button("Plot Period")
-    if submitted:
-        comment = "Comment"
-        incomes = {'Salary': 3000, 'Investments': 400, 'Other Income': 600}
-        expenses = {'Rent': 1500, 'Utilities': 40, 'Food': 200, 'Transportation': 60, 'Insurance': 300,
-                    'Phone': 25, 'Subscriptions': 125, 'Savings': 1000, 'Miscellaneous': 750}
+if selected == "Data Visualization":
+    st.header("Data Visualization")
+    with st.form("saved_periods"):
+        period = st.selectbox("Select Period:", ["June_2023"])
+        submitted = st.form_submit_button("Plot Period")
+        if submitted:
+            comment = "Comment"
+            incomes = {'Salary': 3000, 'Investments': 400, 'Other Income': 600}
+            expenses = {'Rent': 1500, 'Utilities': 40, 'Food': 200, 'Transportation': 60, 'Insurance': 300,
+                        'Phone': 25, 'Subscriptions': 125, 'Savings': 1000, 'Miscellaneous': 750}
 
-        # Create metrics
-        total_income = sum(incomes.values())
-        total_expense = sum(expenses.values())
-        remaining_budget = total_income - total_expense
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Total Income", f"{total_income}{currency}")
-        col2.metric("Total Expense", f"{total_expense}{currency}")
-        col3.metric("Remaining Budget", f"{remaining_budget}{currency}")
-        st.text(f"Comment: {comment}")
+            # Create metrics
+            total_income = sum(incomes.values())
+            total_expense = sum(expenses.values())
+            remaining_budget = total_income - total_expense
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Total Income", f"{total_income}{currency}")
+            col2.metric("Total Expense", f"{total_expense}{currency}")
+            col3.metric("Remaining Budget", f"{remaining_budget}{currency}")
+            st.text(f"Comment: {comment}")
 
-        # Create sankey chart
-        label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
-        source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
-        target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
-        value = list(incomes.values()) + list(expenses.values())
+            # Create sankey chart
+            label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
+            source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+            value = list(incomes.values()) + list(expenses.values())
 
-        # Data to dict, dict to sankey
-        link = dict(source=source, target=target, value=value)
-        node = dict(label=label, pad=20, thickness=30, color="#E694FF")
-        data = go.Sankey(link=link, node=node)
+            # Data to dict, dict to sankey
+            link = dict(source=source, target=target, value=value)
+            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
+            data = go.Sankey(link=link, node=node)
 
-        # Plot it!
-        fig = go.Figure(data)
-        fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
-        st.plotly_chart(fig, use_container_width=True)
+            # Plot it!
+            fig = go.Figure(data)
+            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+            st.plotly_chart(fig, use_container_width=True)
